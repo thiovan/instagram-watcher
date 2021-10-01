9d6df426af3defab5ecafa2c760143f64a0f8655
Thio Van Agusti <thiovan99@gmail.com>
2021-10-01 16:56:03 +0700

    prevent API cache

--- api/base.php
+++ api/base.php
@@ -5,6 +5,9 @@ require __DIR__ . '/../libs/rb-mysql.php';
 date_default_timezone_set('Asia/Jakarta');
 
 header('Content-Type: application/json; charset=utf-8');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 // init dot env
 $DOTENV = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
